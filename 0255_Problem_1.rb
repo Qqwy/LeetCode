38c370bfa0a228d5263f3d9a2c373ee923f2e6c4
Marten/Qqwy <w-m@wmcode.nl>
2021-08-30 00:29:49 +0200

    Idiomatic Ruby solution.
    Courtesy of Luj8n (c.f.
    https://www.youtube.com/watch?v=UVUjnzpQKUo&lc=UgzQOYQEEKckxNfgapR4AaABAg
    )

--- 0255_Problem_1.rb
+++ 0255_Problem_1.rb
@@ -1,5 +1,14 @@
 # Problem Link: https://leetcode.com/contest/weekly-contest-255/problems/find-greatest-common-divisor-of-array/
 
+# Solution shown in video:
 def find_gcd(nums)
     return nums.max.gcd(nums.min)
 end
+
+# More idiomatic solution:
+#   (Courtesy of YouTube user Luj8n, 
+#   c.f. https://www.youtube.com/watch?v=UVUjnzpQKUo&lc=UgzQOYQEEKckxNfgapR4AaABAg )
+def find_gcd2(nums)
+  nums.minmax.reduce(:gcd)
+end
+
