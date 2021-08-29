3b39db3d0ec544719ed7586e8018064d35998dea
Marten/Qqwy <w-m@wmcode.nl>
2021-08-30 00:36:35 +0200

    A second Haskell solution without liftM2

--- 0255_Problem_1.hs
+++ 0255_Problem_1.hs
@@ -2,4 +2,10 @@
 
 import Control.Monad (liftM2)
 
+-- Original solution featured in the video:
 findGCD = liftM2 gcd minimum maximum
+
+-- Note that using `liftA2` (from Control.Applicative) instead of `liftM2` also works and is usually more efficient, as for some types of inputs it can be paralellized.
+
+-- A solution without using `liftM2` or `LiftA2` (to show what is going on under the hood):
+findGCD'' nums = gcd (minimum nums) (maximum nums)
